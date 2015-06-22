8703b3d7d768376cdb2875b2d974585b471bed77
Christopher Ciufo <christopher.ciufo@gmail.com>
2015-06-22 19:13:38 +0900

    Implemented fuse.js fuzzy search functionality. Searching works but only top suggestion can be displayed without errors.

--- background.js
+++ background.js
@@ -6,29 +6,57 @@
 (function () {
   'use strict';
 
-  var topSuggestion = {};
-  var suggestions = [];
+  var fuse;
+  var searchList;
+  var currentContent = "";
+  var previousText = "";
 
-  chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
+  chrome.omnibox.onInputStarted.addListener(function () {
     chrome.management.getAll(function (results) {
-      topSuggestion = {
-        description: "<dim>Launch</dim> " + results[0].name + " - <dim>" + results[0].description + "</dim>"
-      };
-
-      for (var i = 1; i < 5; i++) {
-        suggestions.push({
-          content: results[i].name,
-          description: results[i].name + (results[i].description ? " - <dim>" + results[i].description + "</dim>" : "")
-        });
-      }
-
-      chrome.omnibox.setDefaultSuggestion(topSuggestion);
-      suggest(suggestions);
+      searchList = results;
+      fuse = new Fuse(searchList, {
+        keys:['name', 'description']
+      });
     });
   });
 
+  chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
+    if (text) {
+      var results = fuse.search(text);
+      if (results) {
+        var topSuggestion = {
+          description: "<dim>Launch</dim> " + results[0].name + " - <dim>" + results[0].description + "</dim>"
+        };
+        currentContent = results[0].name;
+        previousText = text;
+
+        chrome.omnibox.setDefaultSuggestion(topSuggestion);
+        
+        // var suggestions = [];
+        // var numItems = Math.min(results.length, 5);
+
+        // for (var i = 1; i < numItems; i++) {
+        //   suggestions.push({
+        //     content: results[i].name,
+        //     description: results[i].name + (results[i].description ? " - <dim>" + results[i].description + "</dim>" : "")
+        //   });
+        // }
+
+        // console.log(numItems);
+        // console.log(suggestions);
+        // suggest(suggestions);
+      }
+    }
+  });
+
   chrome.omnibox.onInputEntered.addListener(function (text, disposition) {
-    alert(text);
+    var searchKey;
+    if (text == previousText) {
+      searchKey = currentContent;
+    } else {
+      searchKey = text;
+    }
+    alert(searchKey);
   }); 
 
 })();
