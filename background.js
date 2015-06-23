061112be9481766db6bbf18febe9788bedcff173
Christopher Ciufo <christopher.ciufo@gmail.com>
2015-06-23 11:29:42 +0900

    Fixed xml escaping issue. Now filtering apps and extensions works and correct suggestion list is displayed.

--- background.js
+++ background.js
@@ -6,48 +6,58 @@
 (function () {
   'use strict';
 
+
+  function escapeText(text) {
+    return text
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+  }
+
+  function generateDescription (result) {
+    return escapeText(result.name) + (escapeText(result.description) ? " - <dim>" + escapeText(result.description) + "</dim>" : "");
+  }
+
   var fuse;
-  var searchList;
   var currentContent = "";
   var previousText = "";
 
-  chrome.omnibox.onInputStarted.addListener(function () {
-    chrome.management.getAll(function (results) {
-      searchList = results;
-      fuse = new Fuse(searchList, {
-        keys:['name', 'description']
-      });
-    });
-  });
-
   chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
-    if (text) {
-      var results = fuse.search(text);
-      if (results) {
-        var topSuggestion = {
-          description: "<dim>Launch</dim> " + results[0].name + " - <dim>" + results[0].description + "</dim>"
-        };
-        currentContent = results[0].name;
-        previousText = text;
-
-        chrome.omnibox.setDefaultSuggestion(topSuggestion);
-        
-        // var suggestions = [];
-        // var numItems = Math.min(results.length, 5);
-
-        // for (var i = 1; i < numItems; i++) {
-        //   suggestions.push({
-        //     content: results[i].name,
-        //     description: results[i].name + (results[i].description ? " - <dim>" + results[i].description + "</dim>" : "")
-        //   });
-        // }
-
-        // console.log(numItems);
-        // console.log(suggestions);
-        // suggest(suggestions);
+    if (text.length) {
+      if (typeof fuse === 'undefined') {
+        chrome.management.getAll(function (searchList) {
+          fuse = new Fuse(searchList, {
+            keys:['name', 'description']
+          });
+          displaySuggestions(text, suggest);
+        });
+      } else {
+        displaySuggestions(text, suggest);
       }
     }
   });
+
+  function displaySuggestions(text, suggest) {
+    var results = fuse.search(text);
+    if (results.length) {
+      var topSuggestion = {
+        description: "<dim>Launch</dim> " + generateDescription(results[0])
+      };
+      currentContent = results[0].name;
+      previousText = text;
+
+      chrome.omnibox.setDefaultSuggestion(topSuggestion);
+      
+      var suggestions = [];
+      for (var i = 1; i < Math.min(results.length, 5); i++) {
+        suggestions.push({
+          content: results[i].name,
+          description: generateDescription(results[i])
+        });
+      }
+      suggest(suggestions);
+    }
+  }
 
   chrome.omnibox.onInputEntered.addListener(function (text, disposition) {
     var searchKey;
